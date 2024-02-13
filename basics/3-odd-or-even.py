525a87a600cf39e59450dc01ed20cd9b0590590f
Ariel Agranovich <ariel.agra@gmail.com>
2024-02-13 14:51:19 +0200

    solved the third exercise

---
+++ basics/3-odd-or-even.py
@@ -0,0 +1,21 @@
+provided_number = int(input("Please provide a number: "))
+
+remainder = provided_number % 2
+
+
+if remainder == 0:
+    print(f"the number {provided_number} is even")
+
+else:
+    print("the number", provided_number, "is odd")
+
+
+# +
+# -
+# *
+# /
+
+
+# **
+# //
+# %
