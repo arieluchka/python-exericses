bf43440fd6358526e3cb3af4b7bd35b1e66208b5
Ariel Agranovich <ariel.agra@gmail.com>
2024-02-13 14:12:27 +0200

    added sum calculator file

---
+++ basics/sum-2-numbers.py
@@ -0,0 +1,9 @@
+
+# 1) Write a program that takes two numbers as input from the user and prints their sum.
+
+num_1 = int(input("choose the first number: "))
+num_2 = int(input("choose the second number: "))
+
+
+
+print(num_1 + num_2)
